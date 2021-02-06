b2f0d20f5a738097aeb46da17c518492086fbeca
Katie Cordes <gillis.katharine@gmail.com>
2021-02-06 13:37:12 -0700

    Stop upgrading auto-upgrade software

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-choco.exe upgrade docker-desktop -y
+if choco.exe list -lo | grep -q 'docker-desktop'; then
+  echo "Docker Desktop already installed - upgrade via Docker Desktop instead of choco"
+else
+  choco.exe install docker-desktop -y
+fi
